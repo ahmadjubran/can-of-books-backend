beaf45175b19de11d23ee52be77d26a737d66dd0
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-08-09 21:43:40 +0300

    Biuld MongoDB

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { handleBooks } = require("./modules/book.js");
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get("/", handleHome);
 
-  response.send('test request received')
+function handleHome(req, res) {
+  res.send("Hello World");
+}
 
-})
+mongoose.connect("mongodb://localhost:27017/books");
+
+app.get("/books", handleBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
